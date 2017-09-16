f7832385becc0280bb313053a3727dd31d27b564
TheNecker <chrisp132012@gmail.com>
2017-09-15 20:16:20 -0400

    pulls for days

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require "sinatra"
 require_relative "bookisbn.rb"
+require 'aws-sdk'
+load './local_env.rb' if File.exist?('./local_env.rb')
+Aws.use_bundled_cert!
 
 get '/' do
 erb :index
@@ -11,18 +14,27 @@ end
 
 post '/enter_isbn' do
 isbn_data = params[:isbn_data].to_s
-isbn_done = isbn_check(isbn_data).to_s
+isbn_done = params[:isbn_done].to_s
+isbn_done = isbn_check(isbn_data)
+p isbn_done
+#isbn_done = isbn_text(isbn_done).to_s
+p isbn_done
 redirect '/isbn_out?isbn_done=' + isbn_done.to_s + '&isbn_data=' + isbn_data.to_s
 end 
 
 get '/isbn_out' do 
 isbn_data = params[:isbn_data]
 isbn_done = params[:isbn_done]
+#push_to_bucket(isbn_data, isbn_done)
+
 erb :eval, :locals => {:isbn_data=>isbn_data, :isbn_done=>isbn_done}
 end
 
 post '/isbn_out' do 
+
 isbn_data = params[:isbn_data]
 isbn_done = params[:isbn_done]
+
 end
+
 

3498d7b3789d0979420d48b4213fa6f35a40db31
TheNecker <chrisp132012@gmail.com>
2017-09-19 09:44:34 -0400

    heroku

--- app.rb
+++ app.rb
@@ -1,22 +1,8 @@
 require "sinatra"
 require_relative "bookisbn.rb"
-require 'pony'
-require 'pg'
-require 'bundler/setup'
-require 'recaptcha'
 require 'aws-sdk'
 load './local_env.rb' if File.exist?('./local_env.rb')
 Aws.use_bundled_cert!
-
-db_params = {
-    host: ENV['host'],
-    port: ENV['port'],
-    dbname: ENV['dbname'],
-    user: ENV['user'],
-    password: ENV['password']
-}
-
-db = PG::Connection.new(db_params)
 
 get '/' do
 erb :index
